5e4d4955079ecad7cf5f71fcf8c409270b22c7ad
sour_leangchhean7 <leangchhean7@gmail.com>
2015-11-19 11:14:21 +0700

    fix error file style and script not found

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,5 +1,15 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {}
