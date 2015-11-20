bfe49eb4f7bb6e8e351d42a6494ee846b76d0d63
khmerupdateku <khmerupdateku@gmail.com>
2015-11-20 10:38:44 +0700

    Add gategory

--- www/js/app/model/category-model.js
+++ www/js/app/model/category-model.js
@@ -16,16 +16,20 @@ var CategoryModel = {
     },
     setCategory: function () {
         CategoryModel.category.push(
-                {'id': '1', "name": "Technology", "image":"img/technology.png"},
-                {'id': '2', "name": "Education" , "image":"img/educationhome.png"},
-                {'id': '3', "name": "Politic", "image":"img/Political.png"},
-                {'id': '4', "name": "Disable Persion", "image":"img/disable.png"} ,
-                {'id': '5', "name": "TRADITIONAL", "image":"img/traditional.png"},
-                {'id': '6', "name": "Children", "image":"img/children.png"},
-                {'id': '7', "name": "Sex traffic Abuse", "image":"img/humanabuse.png"},
-                {'id': '8', "name": "Drug Abuse", "image":"img/drugabuse.png"},
-                {'id': '9', "name": "Human Right", "image":"img/humanright.png"},
-                {'id': '10', "name": "Health", "image":"img/health.png"}
+                {'id': '1', "name": "បច្ចេកវិទ្យា", "image":"img/technology.png"},
+                {'id': '2', "name": "ការអប់រំ" , "image":"img/educationhome.png"},
+                {'id': '3', "name": "នយោបាយ", "image":"img/Political.png"},
+                {'id': '4', "name": "ជនពិការ", "image":"img/disable.png"} ,
+                {'id': '5', "name": "ជំងឺអេដស៏","image":"img/hiv.png"},
+                {'id': '6', "name": "កុមារ", "image":"img/children.png"},
+                {'id': '7', "name": "ការជួញដូរផ្លូវភេទ", "image":"img/humanabuse.png"},
+                {'id': '8', "name": "គ្រឿងញៀនៃ", "image":"img/drugabuse.png"},
+                {'id': '9', "name": "សិទ្ធមនុស្ស", "image":"img/humanright.png"},
+                {'id': '10', "name": "សុខមាលភាព", "image":"img/health.png"},
+                {'id': '11', "name": "ស្ត្រី", "image":"img/women.png"},
+                {'id': '12', "name": "ដីធ្លី", "image":"img/land.png"},
+                {'id': '13', "name": "សាសនា", "image":"img/butha.jpg"}
+                        
         );
     },
     getCategory: function () {
