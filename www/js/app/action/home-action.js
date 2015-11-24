9767ee8cb9a1f5861434562afe0fc92ddc411eb5
yaiyai123crak <sokchanny.sg@gmail.com>
2015-11-24 14:14:30 +0700

    and ngo number

--- www/js/app/action/home-action.js
+++ www/js/app/action/home-action.js
@@ -6,6 +6,9 @@ $(function () {
         CategoryModel.setId(this.id);        
         CategoryModel.setName($(this).attr("data-cat_name"));        
     });
+    $(document).delegate("#header-home", "click", function () {
+        alert("s");
+    });
 });
 
 
